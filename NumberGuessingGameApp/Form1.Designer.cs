b1e2c4eb01ec4097c85e885c97880205d94c3b16
sbuzzard7 <s.buzzard7@outlook.com>
2024-12-12 13:01:17 -0500

    Add files via upload

---
+++ NumberGuessingGameApp/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace NumberGuessingGameApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.txtEnterNum = new System.Windows.Forms.TextBox();
+            this.lblQuestion = new System.Windows.Forms.Label();
+            this.lblGuessed = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCheck
+            // 
+            resources.ApplyResources(this.btnCheck, "btnCheck");
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.checkNumber);
+            // 
+            // txtEnterNum
+            // 
+            this.txtEnterNum.Cursor = System.Windows.Forms.Cursors.Arrow;
+            resources.ApplyResources(this.txtEnterNum, "txtEnterNum");
+            this.txtEnterNum.Name = "txtEnterNum";
+            this.txtEnterNum.TextChanged += new System.EventHandler(this.txtEnterNum_TextChanged);
+            // 
+            // lblQuestion
+            // 
+            resources.ApplyResources(this.lblQuestion, "lblQuestion");
+            this.lblQuestion.BackColor = System.Drawing.Color.Transparent;
+            this.lblQuestion.Name = "lblQuestion";
+            this.lblQuestion.Click += new System.EventHandler(this.lblQuestion_Click);
+            // 
+            // lblGuessed
+            // 
+            resources.ApplyResources(this.lblGuessed, "lblGuessed");
+            this.lblGuessed.BackColor = System.Drawing.Color.Transparent;
+            this.lblGuessed.Name = "lblGuessed";
+            this.lblGuessed.Click += new System.EventHandler(this.lblGuessed_Click);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.Controls.Add(this.lblGuessed);
+            this.Controls.Add(this.lblQuestion);
+            this.Controls.Add(this.txtEnterNum);
+            this.Controls.Add(this.btnCheck);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.TextBox txtEnterNum;
+        private System.Windows.Forms.Label lblQuestion;
+        private System.Windows.Forms.Label lblGuessed;
+    }
+}
+
---
+++ NumberGuessingGameApp/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NumberGuessingGameApp
+{
+    public partial class Form1 : Form
+    {
+
+        Random randomNumber = new Random();
+
+        int number = 0;
+        int guesses = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            loadQuestions();
+        }
+
+        private void checkNumber(object sender, EventArgs e)
+        {
+       
+            int i = Convert.ToInt32(txtEnterNum.Text);
+
+            guesses += 1;
+            lblGuessed.Text = "You guessed " + guesses + " times."; 
+
+            if (i == number)
+            {
+                MessageBox.Show("Nice, you guessed it! Try another?");
+                loadQuestions();
+                txtEnterNum.Text = "";
+                guesses = 0;
+                lblGuessed.Text = "You guessed " + guesses + " times.";
+            }
+            else if (i < number)
+            {
+                MessageBox.Show("Go higher");
+                txtEnterNum.Text = "";
+            }
+            else
+            {
+                MessageBox.Show("Go lower");
+                txtEnterNum.Text = "";
+            }
+
+
+
+        }
+
+        private void loadQuestions()
+        {
+            number = randomNumber.Next(0, 100);
+
+            lblQuestion.Text = "I am thinking of a number between 0 and 100 ";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtEnterNum_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblQuestion_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblGuessed_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
